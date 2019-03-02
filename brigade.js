0e745c294401b89f98fbe01de6047a073be96d5c
Timm Hirsens <timm.hirsens@dvag.com>
2019-03-02 16:48:33 +0100

    Brigade build

--- brigade.js
+++ brigade.js
@@ -1,5 +1,14 @@
-const { events } = require("brigadier");
+const { events, Job } = require("brigadier");
 
 events.on("push", function(e, project) {
   console.log("received push for commit " + e.revision.commit)
+
+  const job = new Job("gradle", "openjdk:11.0.2-jdk-slim-stretch");
+
+  job.tasks = [
+    "./gradlew build"
+  ]
+
+  job.run();
+
 })
