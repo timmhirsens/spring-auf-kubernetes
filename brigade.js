57771a9607a95e9ee100789549f4084b24540bb7
Timm Hirsens <timm.hirsens@dvag.com>
2019-03-04 14:46:00 +0100

    docker build brigadier

--- brigade.js
+++ brigade.js
@@ -1,15 +1,27 @@
 const { events, Job } = require("brigadier");
 
 events.on("push", function(e, project) {
-  console.log("received push for commit " + e.revision.commit)
+  console.log("received push for commit " + e.revision.commit);
 
-  const job = new Job("gradle", "openjdk:11.0.2-jdk-slim-stretch");
+  const dockerTag = `fr1zle/spring-auf-kubernetes:${e.revision.commit.substring(
+    0,
+    8
+  )}`;
 
-  job.tasks = [
-    "cd src",
-    "./gradlew build"
-  ]
+  const gradleBuild = new Job("gradle", "openjdk:11.0.2-jdk-slim-stretch");
+  gradleBuild.tasks = ["cd src", "./gradlew build"];
+  gradleBuild.resourceRequests.memory = "512Mi";
+  gradleBuild.resourceRequests.cpu = "100m";
+  gradleBuild.resourceLimits.memory = "1Gi";
+  gradleBuild.resourceLimits.cpu = "1";
 
-  job.run();
+  const dockerBuild = new Job("docker", "docker:18.09.3-dind");
+  dockerBuild.task = ["cd src", `docker build . -t ${dockerTag}`];
+  dockerBuild.resourceRequests.memory = "200Mi";
+  dockerBuild.resourceRequests.cpu = "100m";
+  dockerBuild.resourceLimits.memory = "400Mi";
+  dockerBuild.resourceLimits.cpu = "200m";
 
-})
+  gradleBuild.run();
+  dockerBuild.run();
+});
